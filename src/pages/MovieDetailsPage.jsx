19bb149226f07fe56fa9601373b90423ef3e6a5a
Serhii Kondratenko <kondratenko.serhii3@gmail.com>
2023-09-04 02:05:14 +0300

    Rename page

---
+++ src/pages/MovieDetailsPage.jsx
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Link, Outlet, useLocation, useParams } from 'react-router-dom';
+
+const MovieDetails = () => {
+  const [singleMovie, setSingleMovie] = useState(null);
+  const { movieId } = useParams();
+  const location = useLocation();
+
+  useEffect(() => {
+    const key = 'a8702b4fc1615ccb68ca9d5f4ec2dee9';
+
+    axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${key}`)
+      .then(response => {
+        setSingleMovie(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, [movieId]);
+
+  return (
+    <>
+      {singleMovie && (
+        <div>
+          <Link to={location.state.from}>Go back</Link>
+          <img src={singleMovie.poster_path} alt={singleMovie.original_title}></img>
+          <h2>
+            {singleMovie.original_title}({singleMovie.release_date})
+          </h2>
+          <p>User Score:{singleMovie.vote_average}%</p>
+          <h3>Overview</h3>
+          <p>{singleMovie.overview}</p>
+          <h3>Genres</h3>
+          <ul>
+            {singleMovie.genres.map(genre => (
+              <li key={genre.id}>{genre.name}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+        <p>Additional Information</p>
+      <ul>
+        <li>
+          <Link to="cast">Cast</Link>
+        </li>
+        <li>
+          <Link to="reviews">Reviews</Link>
+        </li>
+      </ul>
+      <Outlet />
+    </>
+  );
+};
+export default MovieDetails;
